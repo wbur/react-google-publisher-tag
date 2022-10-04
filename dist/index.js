5bb38ca6c1d980314f32c04e01d402071950eeef
Christopher Bowers <bowersc@bu.edu>
2022-10-04 19:07:48 +0000

    Added UNSAFE_componentWillReceiveProps and updated React peerDeps

--- dist/index.js
+++ dist/index.js
@@ -5,22 +5,34 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+Object.defineProperty(exports, "Ad", {
+  enumerable: true,
+  get: function () {
+    return _GooglePublisherTag.default;
+  }
+});
 Object.defineProperty(exports, "Dimensions", {
   enumerable: true,
   get: function () {
-    return _Dimensions2.default;
+    return _Dimensions.default;
   }
 });
 Object.defineProperty(exports, "Format", {
   enumerable: true,
   get: function () {
-    return _Format2.default;
+    return _Format.default;
   }
 });
-Object.defineProperty(exports, "Ad", {
+Object.defineProperty(exports, "Horizontal", {
   enumerable: true,
   get: function () {
-    return _GooglePublisherTag.default;
+    return _Horizontal.default;
+  }
+});
+Object.defineProperty(exports, "Mobile", {
+  enumerable: true,
+  get: function () {
+    return _Mobile.default;
   }
 });
 Object.defineProperty(exports, "default", {
@@ -29,26 +41,14 @@ Object.defineProperty(exports, "default", {
     return _GooglePublisherTag.default;
   }
 });
-Object.defineProperty(exports, "Mobile", {
-  enumerable: true,
-  get: function () {
-    return _Mobile2.default;
-  }
-});
-Object.defineProperty(exports, "Horizontal", {
-  enumerable: true,
-  get: function () {
-    return _Horizontal2.default;
-  }
-});
 
-var _Dimensions2 = _interopRequireDefault(require("./constants/Dimensions"));
+var _Dimensions = _interopRequireDefault(require("./constants/Dimensions"));
 
-var _Format2 = _interopRequireDefault(require("./constants/Format"));
+var _Format = _interopRequireDefault(require("./constants/Format"));
 
 var _GooglePublisherTag = _interopRequireDefault(require("./GooglePublisherTag"));
 
-var _Mobile2 = _interopRequireDefault(require("./Mobile"));
+var _Mobile = _interopRequireDefault(require("./Mobile"));
 
-var _Horizontal2 = _interopRequireDefault(require("./Horizontal"));
+var _Horizontal = _interopRequireDefault(require("./Horizontal"));
 //# sourceMappingURL=index.js.map
